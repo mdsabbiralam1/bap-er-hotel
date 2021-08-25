3fe9bdab63eb7ae71c9de1ba6b4d81883eb1c844
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-25 13:29:12 +0600

    banking 2 practise file all design added

--- js/banking.js
+++ js/banking.js
@@ -3,20 +3,24 @@
 document.getElementById('deposit-btn').addEventListener('click', function () {
     const depositInput = document.getElementById('deposit-input');
     const newdepositText = depositInput.value;
-    const newdepositAmoumt = parseFloat(newdepositText);
+    const newdepositAmount = parseFloat(newdepositText);
 
     // update deposit total
     const depositTotal = document.getElementById('deposit-total');
 
     const previousdepositText = depositTotal.innerText;
     const previousdepositAmount = parseFloat(previousdepositText);
-    const newdepositTotal = previousdepositAmount + newdepositAmoumt;
+    const newdepositTotal = previousdepositAmount + newdepositAmount;
     depositTotal.innerText = newdepositTotal;
 
     // update account balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
 
+    const previousBalanceTotal = parseFloat(balanceTotaltext);
+    const newBalanceTotal = previousBalanceTotal + newdepositAmount;
 
-
+    balanceTotal.innerText = newBalanceTotal;
 
     // Clear the deposit input field 
     depositInput.value = ('')
