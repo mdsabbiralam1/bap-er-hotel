3c94f45b15e86a38eabd66bbb6d905125d7b4c07
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-09-09 00:40:22 +0600

    added deposit balance add

--- js/banking2.js
+++ js/banking2.js
@@ -12,14 +12,24 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 
 document.getElementById('deposit-button').addEventListener('click', function () {
     const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
+
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
 
 
     // get current balance
     const depositTotal = document.getElementById('deposit-total');
     const depositTotalText = depositTotal.innerText;
-    depositTotal.innerText = depositAmount;
-    console.log(depositTotalText);
+    const previousTotalAmount = parseFloat(depositTotalText);
+
+    depositTotal.innerText = previousTotalAmount + depositAmount;
+
+    // ubdate balance total
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = previousBalanceTotal + depositAmount;
 
     // clear deposit input field
     depositInput.value = '';
